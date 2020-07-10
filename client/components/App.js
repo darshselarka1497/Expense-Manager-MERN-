751383a50f3b9d503a63336290ad00750d6742ec
dselarka <dselarka@iu.edu>
2020-07-09 22:06:09 -0400

    fix attempt 1

--- client/components/App.js
+++ client/components/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+//client/components/App.js
+import React, { Component } from 'react';
 import '../css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
-
 export default App;
