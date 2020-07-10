a379fbcc6bd3852569e814daf86778b5c05a141f
dselarka <dselarka@iu.edu>
2020-07-09 20:54:32 -0400

    Babel-webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,18 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'client'),
         filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /.jsx?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015','react']
+            }
+        },{
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        }]
     }
 }
