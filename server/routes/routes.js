c03a4f8190fef9b5fc2bfb51afd9fe7ab13e862b
dselarka <dselarka@iu.edu>
2020-07-20 22:18:29 -0400

    Configurating Routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,8 @@
 //server/routes/routes.js
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var Expense = require('./models/Expense');
 router.get('/', function(req, res){
   res.render('index')
 });
