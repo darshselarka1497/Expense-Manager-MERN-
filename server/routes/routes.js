ef103c052ee13377e59c8140bba7d6c746aca6a6
dselarka <dselarka@iu.edu>
2020-07-20 22:33:17 -0400

    Router delete and getAll routes.js

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -32,6 +32,39 @@ router.route('/update')
    year: req.body.year
  };
  console.loc(doc);
- 
-})
+  Expense.update({_id: req.body._id}, doc, function(err,result){
+    if(err)
+      res.send(err);
+    res.send('Expense updated in the db!')
+  });
+});
+
+router.get('/delete', function(req,res){
+  var id = req.query.id;
+  Expense.find({_id:id}).remove().exec(function(err,expense){
+    if(err)
+      res.send((err))
+    res.send('Expense deleted from the db!');
+  })
+});
+
+router.get('/getAll', function(req,res){
+  var monthRec = req.query.month;
+  var yearRec = req.query.year;
+  if(monthRec && monthRec != 'All'){
+    Expense.find({$and:[ {month: monthRec}, {year: yearRec}]}, function(err,expenses){
+      if(err)
+        res.send(err);
+      res.json(expenses);
+    });
+  }
+  else{
+    Expense.find({year: yearRec}, function(err,expenses){
+      if(err)
+        res.send(err)
+      res.json(expenses);
+    });
+  }
+});
+
 module.exports = router;
