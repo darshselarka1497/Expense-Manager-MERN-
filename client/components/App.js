1e1278c8fb85054809d01c19296659576b7c6399
dselarka <dselarka@iu.edu>
2020-07-20 22:00:41 -0400

    Added model (mongoose) for mongoDB

--- client/components/App.js
+++ client/components/App.js
@@ -5,14 +5,7 @@ import '../css/App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <h1>Hello, dis iz my xpenz managerrrrr</h1>
     );
   }
 }
